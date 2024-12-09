37cf1461f7e77cfb1ac25e6a04ab42ae8a63908e
ihssaneah <93397246+ihssaneah@users.noreply.github.com>
2024-12-09 21:43:10 +0100

    Linting update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://ihssanethats-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-    ]
+    (
+        'https://ihssanethats-8000.theiadockernext-1-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    )
+]
+
 CSRF_TRUSTED_ORIGINS = [
-    'https://ihssanethats-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://ihssanethats-8000.theiadockernext-1-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    )
     ]
 
 REST_FRAMEWORK = {
